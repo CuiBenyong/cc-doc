d9508c32ffdb9149571ca840080d11de2f4212c2
CuiBenyong <shl1018@hotmail.com>
2024-03-22 18:51:55 +0800

    add

---
+++ .netlify/functions/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+import serverless from "serverless-http";
+
+const path = require('path');
+const port = 3000;
+
+// Umi static files (including SSG pages)
+app.use(express.static(path.join(__dirname, '../../dist')));
+app.use(express.static(path.join(__dirname, '../../server')));
+
+// Logger middleware
+app.use(async (req, res, next) => {
+  console.info(`${req.method} ${req.url}`);
+  next();
+});
+
+// Umi SSR middleware
+app.use(require(__dirname + '../../server/umi.server').default);
+
+
+
+
+
+// Start server
+// app.listen(port, () => {
+//   console.log(`Server is now running at http://127.0.0.1:${port}/`);
+// });
+export const handler = serverless(app);
